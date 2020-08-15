fb665884b423cac750359f879be26cf977a4ee69
Aracelli Galvez Leon <68877712+Aracelli08@users.noreply.github.com>
2020-08-15 16:39:55 -0500

    Add files via upload

---
+++ js/jst.js
@@ -0,0 +1,23 @@
+/*MENU*/
+let btnMenu = document.querySelector('.btn-menu');
+let menu = document.querySelector('.list-container');
+let containerMenu = document.querySelector('menu');
+let activador = true;
+
+btnMenu.addEventListener('click', () => {
+    
+    if(activador){
+
+        menu.style.left = "0";
+        menu.style.transition = "0.5s";
+
+        activador = false;
+    }else{
+        activador = false;
+
+        menu.style.left = "-100%";
+        menu.style.transition = "0.5s";
+
+        activador = true;
+    }
+});
